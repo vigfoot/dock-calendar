81dee5a8bb3e81f6d9f19e580400eca33a8d379a
Xasquatch <bigfoot7774@gmail.com>
2025-06-23 08:38:03 +0900

    time

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 const unit = ' seconds';
 setInterval(() => {
+    let date = new Date();
+    document.querySelector('#real-time > .hours').innerText = date.getHours();
+    document.querySelector('#real-time > .minutes').innerText = date.getMinutes();
+    document.querySelector('#real-time > .seconds').innerText = date.getSeconds();
+
     const reloadTimeNode = document.getElementById('reload-time');
     if (reloadTimeNode.innerText === 0 + unit || reloadTimeNode.innerText === '') {
         reloadTimeNode.innerText = 60 + unit;
