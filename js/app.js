0845ee913d6b93c6b5e6685a1655c87d9531833a
Xasquatch <bigfoot7774@gmail.com>
2025-06-23 07:55:24 +0900

    app init

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-setInterval(() => location.reload(), 1000 * 60);
+const unit = ' seconds';
+setInterval(() => {
+    const reloadTimeNode = document.getElementById('reload-time');
+    if (reloadTimeNode.innerText === 0 + unit) {
+        reloadTimeNode.innerText = 60 + unit;
+        location.reload();
+
+    } else {
+        reloadTimeNode.innerText = Number(reloadTimeNode.innerText?.replace(unit, '')) - 1 + unit
+
+    }
+}, 1000);
