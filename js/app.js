7dd5d5ad6064a94d57c88d68b5a93b0dcc42bfca
Xasquatch <bigfoot7774@gmail.com>
2025-10-20 11:10:34 +0900

    아 집중안되네

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 const unit = ' seconds';
 setInterval(() => {
     let date = new Date();
-    document.querySelector('#real-time > .hours').innerText = date.getHours();
-    document.querySelector('#real-time > .minutes').innerText = date.getMinutes();
-    document.querySelector('#real-time > .seconds').innerText = date.getSeconds();
+    document.querySelector('.real-time > .hours').innerText = date.getHours();
+    document.querySelector('.real-time > .minutes').innerText = date.getMinutes();
+    document.querySelector('.real-time > .seconds').innerText = date.getSeconds();
 
-    document.querySelector('#real-time-korea > .hours').innerText = date.getUTCHours() + 9 > 24 ? date.getUTCHours() + 9 - 24 : date.getUTCHours() + 9;
-    document.querySelector('#real-time-korea > .minutes').innerText = date.getUTCMinutes();
-    document.querySelector('#real-time-korea > .seconds').innerText = date.getUTCSeconds();
+    document.querySelector('.real-time.korea > .hours').innerText = date.getUTCHours() + 9 >= 24 ? date.getUTCHours() + 9 - 24 : date.getUTCHours() + 9;
+    document.querySelector('.real-time.korea > .minutes').innerText = date.getUTCMinutes();
+    document.querySelector('.real-time.korea > .seconds').innerText = date.getUTCSeconds();
 
-    document.querySelectorAll('#real-time > .time-unit, #real-time-korea > .time-unit').forEach((node, key) => {
+    document.querySelectorAll('.real-time > .time-unit, .real-time.korea > .time-unit').forEach((node, key) => {
         if (node.innerText.length === 2) return;
         node.innerText = '0' + node.innerText;
     });
@@ -17,12 +17,10 @@ setInterval(() => {
 }, 1000);
 
 
-
 let xhr = new XMLHttpRequest();
 xhr.open('GET', '');
 xhr.onreadystatechange = res => {
     if (xhr.readyState !== xhr.DONE || xhr.status > 210) return;
-
 
 
 }
